93ec0c55d67a1ee68df26ff92ffc79c38d97bd8e
Erin Schneider <erinsschneider@gmail.com>
2025-02-10 11:55:02 -0600

    web scraper complete

--- python-web-scraper/scraper.py
+++ python-web-scraper/scraper.py
@@ -1,8 +1,41 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from helium import *
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
 
-website = 'https://hsmo.org/adopt/'
-path = r'C:\Users\erins\Chromedriver\chromedriver-win64\chromedriver.exe'
-service = Service(path)
-driver = webdriver.Chrome(service=service)
-driver.get(website)
+
+url = 'https://www.needypaws.org/animals/list'
+browser = start_chrome(url, headless=False)
+
+
+time.sleep(15)  
+
+
+soup = BeautifulSoup(browser.page_source, 'html.parser')
+
+kill_browser()
+
+
+rows = soup.find_all('tr') 
+
+dog_list = []
+
+
+for row in rows:
+    columns = row.find_all('td', class_='portalTableValue')
+    
+    if len(columns) >= 3:  
+        name = columns[0].text.strip()  
+        breed = columns[1].text.strip()  
+        gender = columns[2].text.strip()  
+        
+        dog_list.append([name, breed, gender])
+
+
+df = pd.DataFrame(dog_list, columns=['Name', 'Breed', 'Gender'])
+
+
+df.to_csv("dog_listings.csv", index=False)
+
+
+print(df.head())
